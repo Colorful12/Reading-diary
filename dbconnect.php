1ac4f62255f38bb17e999bb42f7a9a9a4b7ba1ee
Saki Kohata <cat37280@gmail.com>
2022-03-24 05:45:01 +0900

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO("mysql:dbname=tb231074db; host=localhost", "tb-231074", "rUrp8BEX63", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+    $pdo = new PDO(セキュリティの関係で削除しています);
     $sql ="CREATE TABLE IF NOT EXISTS mydb(
     num INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(32) NOT NULL,
